3dc64edabf2d41c0af7d045eaf2ef872615b2449
Daniel Blanco <daniel@blancomartin.cl>
2018-11-05 16:47:33 -0300

    [WIP] remove aeroo_aeroo as dependencies for l10n_cl_account_vat_ledger

--- l10n_ar_account_vat_ledger/__manifest__.py
+++ l10n_ar_account_vat_ledger/__manifest__.py
@@ -8,7 +8,7 @@
     'summary': '',
     "depends": [
         # TODO we should try to get this report with another tool, not aeroo
-        "report_aeroo",
+        # "report_aeroo",
         "l10n_ar_account",
     ],
     'external_dependencies': {
@@ -16,7 +16,7 @@
     "data": [
         'data/l10n_ar_account_vat_ledger_data.xml',
         'views/account_vat_report_views.xml',
-        'report/account_vat_ledger_report.xml',
+        #  'report/account_vat_ledger_report.xml',
         'wizards/res_config_settings_views.xml',
         'security/security.xml',
         'security/ir.model.access.csv',
