749a6dabcca8f48c81c75bb0d6ad14969625b78d
Daniel Blanco <daniel@blancomartin.cl>
2018-11-05 17:21:43 -0300

    [WIP] remove dependency of aeroo inn account_withholding [WIP] in commit 3dc64ed: also, added dependencies as submodules (odoo.sh)

--- l10n_ar_account_withholding/__manifest__.py
+++ l10n_ar_account_withholding/__manifest__.py
@@ -28,7 +28,7 @@
         'views/account_payment_group_view.xml',
         'views/afip_tabla_ganancias_escala_view.xml',
         'views/afip_tabla_ganancias_alicuotasymontos_view.xml',
-        'views/account_payment_view.xml',
+        # 'views/account_payment_view.xml',
         'views/res_partner_view.xml',
         'security/ir.model.access.csv',
         'security/security.xml',
